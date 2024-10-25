96ef2e1a0c008db7c5cac28db34923359f30f0ae
andre.oliveira <andre.oliveira@mollie.com>
2024-10-25 13:51:02 +0100

    Added match_gender comment

--- app/database/models/GenderEnum.py
+++ app/database/models/GenderEnum.py
@@ -7,6 +7,18 @@ class GenderEnum(Enum):
     PREFER_NOT_TO_SAY = 'Prefiro não dizer'
 
 def match_gender(value: str) -> GenderEnum:
+    """
+    Find the GenderEnum that corresponds to the value
+
+    Parameters:
+        value (str): The enum value to be matched
+
+    Returns:
+        (GenderEnum): The GenderEnum that corresponds to the given value
+
+    Throws:
+        (ValueError): If no match is found
+    """
     for gender in GenderEnum:
         if gender.value.lower() == value.lower():
             return gender
