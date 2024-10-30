53c9d6cc833176deb1264f4e00f734a1f63c9216
andre.oliveira <andre.oliveira@mollie.com>
2024-10-30 11:07:52 +0000

    Fix validator tests

--- tests/validator/test_validator.py
+++ tests/validator/test_validator.py
@@ -1,231 +1,166 @@
 from app.validator.validator import Validator
 
 class TestValidator:
-    def test_validate_first_name(self, first_name: str):
+    def test_validate_normal_string(self, first_name: str):
         """
         Check if the field is valid
         """
-        success, message = Validator.first_name(first_name)
+        errors = Validator.validate_payload(
+            {'normal_string': first_name}
+        )
 
-        assert success
-        assert not message
+        assert not errors
 
 
-    def test_validate_empty_first_name(self, empty_field: str):
+    def test_validate_empty_string(self, empty_field: str):
         """
         Check if the field is valid
         """
-        success, message = Validator.first_name(empty_field)
+        errors = Validator.validate_payload({
+            'empty_string': empty_field
+        })
 
-        assert not success
-        assert message
-
-
-    def test_validate_last_name(self, last_name: str):
-        """
-        Check if the field is valid
-        """
-        success, message = Validator.last_name(last_name)
-
-        assert success
-        assert not message
-
-
-    def test_validate_empty_last_name(self, empty_field: str):
-        """
-        Check if the field is valid
-        """
-        success, message = Validator.last_name(empty_field)
-
-        assert not success
-        assert message
-
-
-    def test_validate_username(self, username: str):
-        """
-        Check if the field is valid
-        """
-        success, message = Validator.username(username)
-
-        assert success
-        assert not message
-
-
-    def test_validate_empty_username(self, empty_field: str):
-        """
-        Check if the field is valid
-        """
-        success, message = Validator.username(empty_field)
-
-        assert not success
-        assert message
+        assert len(errors) == 1
 
 
     def test_validate_email(self, email: str):
         """
         Check if the field is valid
         """
-        success, message = Validator.email(email)
+        errors = Validator.validate_payload({
+            'email': email
+        })
 
-        assert success
-        assert not message
-
-
-    def test_validate_empty_email(self, empty_field: str):
-        """
-        Check if the field is valid
-        """
-        success, message = Validator.email(empty_field)
-
-        assert not success
-        assert message
+        assert not errors
 
 
     def test_validate_invalid_email(self, invalid_email: str):
         """
         Check if the field is valid
         """
-        success, message = Validator.email(invalid_email)
+        errors = Validator.validate_payload({
+            'email': invalid_email
+        })
 
-        assert not success
-        assert message
+        assert len(errors) == 1
 
 
     def test_validate_date(self, date: str):
         """
         Check if the field is valid
         """
-        success, message = Validator.date(date)
+        errors = Validator.validate_payload({
+            'date': date
+        })
 
-        assert success
-        assert not message
-
-
-    def test_validate_empty_date(self, empty_field: str):
-        """
-        Check if the field is valid
-        """
-        success, message = Validator.date(empty_field)
-
-        assert not success
-        assert message
+        assert not errors
 
 
     def test_validate_invalid_value_date(self, invalid_value_date: str):
         """
         Check if the field is valid
         """
-        success, message = Validator.date(invalid_value_date)
+        errors = Validator.validate_payload({
+            'date': invalid_value_date
+        })
 
-        assert not success
-        assert message
+        assert len(errors) == 1
 
 
     def test_validate_invalid_format_date(self, invalid_format_date: str):
         """
         Check if the field is valid
         """
-        success, message = Validator.date(invalid_format_date)
+        errors = Validator.validate_payload({
+            'date': invalid_format_date
+        })
 
-        assert not success
-        assert message
+        assert len(errors) == 1
 
 
     def test_validate_gender(self, gender: str):
         """
         Check if the field is valid
         """
-        success, message = Validator.gender(gender)
+        errors = Validator.validate_payload({
+            'gender': gender
+        })
 
-        assert success
-        assert not message
-
-
-    def test_validate_empty_gender(self, empty_field: str):
-        """
-        Check if the field is valid
-        """
-        success, message = Validator.gender(empty_field)
-
-        assert not success
-        assert message
+        assert not errors
 
 
     def test_validate_invalid_gender(self, invalid_gender: str):
         """
         Check if the field is valid
         """
-        success, message = Validator.gender(invalid_gender)
+        errors = Validator.validate_payload({
+            'gender': invalid_gender
+        })
 
-        assert not success
-        assert message
+        assert len(errors) == 1
 
 
     def test_validate_password(self, password: str):
         """
         Check if the field is valid
         """
-        success, message = Validator.password(password)
+        errors = Validator.validate_payload({
+            'password': password
+        })
 
-        assert success
-        assert not message
-
-
-    def test_validate_empty_password(self, empty_field: str):
-        """
-        Check if the field is valid
-        """
-        success, message = Validator.password(empty_field)
-
-        assert not success
-        assert message
+        assert not errors
 
 
     def test_validate_invalid_small_password(self, invalid_small_password: str):
         """
         Check if the field is valid
         """
-        success, message = Validator.password(invalid_small_password)
+        errors = Validator.validate_payload({
+            'password': invalid_small_password
+        })
 
-        assert not success
-        assert message
+        assert len(errors) == 1
 
 
     def test_validate_invalid_upper_password(self, invalid_upper_password: str):
         """
         Check if the field is valid
         """
-        success, message = Validator.password(invalid_upper_password)
+        errors = Validator.validate_payload({
+            'password': invalid_upper_password
+        })
 
-        assert not success
-        assert message
+        assert len(errors) == 1
 
 
     def test_validate_invalid_lower_password(self, invalid_lower_password: str):
         """
         Check if the field is valid
         """
-        success, message = Validator.password(invalid_lower_password)
+        errors = Validator.validate_payload({
+            'password': invalid_lower_password
+        })
 
-        assert not success
-        assert message
+        assert len(errors) == 1
 
 
     def test_validate_invalid_number_password(self, invalid_number_password: str):
         """
         Check if the field is valid
         """
-        success, message = Validator.password(invalid_number_password)
+        errors = Validator.validate_payload({
+            'password': invalid_number_password
+        })
 
-        assert not success
-        assert message
+        assert len(errors) == 1
 
 
     def test_validate_invalid_special_char_password(self, invalid_special_char_password: str):
         """
         Check if the field is valid
         """
-        success, message = Validator.password(invalid_special_char_password)
+        errors = Validator.validate_payload({
+            'password': invalid_special_char_password
+        })
 
-        assert not success
-        assert message
+        assert len(errors) == 1
