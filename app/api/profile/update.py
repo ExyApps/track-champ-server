60e7437f6df1cdca87ead7f4417d47e5b3117125
andre.oliveira <andre.oliveira@mollie.com>
2024-11-05 13:58:43 +0000

    Fix update profile problem

--- app/api/profile/update.py
+++ app/api/profile/update.py
@@ -1,6 +1,6 @@
 from . import profile_bp
-import base64, os, re
-from flask import request, jsonify
+import base64, os
+from flask import request, jsonify, g
 from http import HTTPStatus
 
 from app.database.wrapper import authentication
@@ -12,6 +12,9 @@ def update():
     """
     Update the profile
     """
+    if not g.user_id:
+        return {'success': False, 'detail': 'Não tem uma sessão iniciada'}, HTTPStatus.TEMPORARY_REDIRECT
+
     payload = request.json
 
     if 'gender' in payload:
@@ -19,12 +22,12 @@ def update():
 
     profile_image = payload['profile_image']
 
-    image_path = f'files/profile_images/user{payload["id"]}.png'
+    image_path = f'files/profile_images/user{g.user_id}.png'
     if profile_image is not None:
         img_type, content = profile_image.split(',')
         with open(image_path, "wb") as fh:
             fh.write(base64.b64decode(payload['profile_image']))
-        payload['profile_image'] = f'files/profile_images/user{payload["id"]}.png'
+        payload['profile_image'] = f'files/profile_images/user{g.user_id}.png'
     else:
         if os.path.exists(image_path):
             os.remove(image_path)
