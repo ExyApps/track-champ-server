923e73f43b67e35048632a2125574ee0fde3d27d
andre.oliveira <andre.oliveira@mollie.com>
2024-11-05 16:06:24 +0000

    Attempt to deploy

--- app.py
+++ app.py
@@ -6,34 +6,33 @@ from config import Config, TestConfig
 
 from src.initializer import create_app
 
-if __name__ == "__main__":
-    if os.environ.get('FLASK_ENV') == 'development':
-        app = create_app(config_class=TestConfig)
-    else:
-        app = create_app()
+if os.environ.get('FLASK_ENV') == 'development':
+    app = create_app(config_class=TestConfig)
+else:
+    app = create_app()
 
-    # ERROR HANDLING
-    # @app.errorhandler(KeyError)
-    # def handle_key_error(e):
-    #     """
-    #     Catch the error if there is a required field missing in the request
-    #     """
-    #     app.logger.exception('An unhandled KeyError exception occured')
+# ERROR HANDLING
+@app.errorhandler(KeyError)
+def handle_key_error(e):
+    """
+    Catch the error if there is a required field missing in the request
+    """
+    app.logger.exception('An unhandled KeyError exception occured')
 
-    #     return jsonify({
-    #         'message': 'Pedido inválido, confirme que envia toda a informação necessária'
-    #     }), HTTPStatus.BAD_REQUEST
+    return jsonify({
+        'message': 'Pedido inválido, confirme que envia toda a informação necessária'
+    }), HTTPStatus.BAD_REQUEST
 
 
-    # @app.errorhandler(Exception)
-    # def handle_error(e):
-    #     """
-    #     Catch any error that was not desired
-    #     """
-    #     app.logger.exception('An unhandled exception occured')
+@app.errorhandler(Exception)
+def handle_error(e):
+    """
+    Catch any error that was not desired
+    """
+    app.logger.exception('An unhandled exception occured')
 
-    #     return jsonify({
-    #         'message': 'Algo inesperado aconteceu, pedimos desculpa pelo incómodo'
-    #     }), HTTPStatus.INTERNAL_SERVER_ERROR
+    return jsonify({
+        'message': 'Algo inesperado aconteceu, pedimos desculpa pelo incómodo'
+    }), HTTPStatus.INTERNAL_SERVER_ERROR
 
-    app.run()
+# app.run()
