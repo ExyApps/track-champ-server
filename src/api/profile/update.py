ad65a3fda1e69cedfc4014389945090e3c8632f1
andre.oliveira <andre.oliveira@mollie.com>
2024-11-07 16:55:31 +0000

    Another attempt

--- src/api/profile/update.py
+++ src/api/profile/update.py
@@ -22,15 +22,17 @@ def update():
 
     profile_image = payload['profile_image']
 
-    image_path = f'static/images/user_{g.user_id}.png'
+    image_path = f'static/images/user_{g.user_id}'
     if profile_image is not None:
         img_type, _ = profile_image.split(',')
+        image_path += img_type.split(';')[0].split('/')[1]
         with open(image_path, "wb") as fh:
             fh.write(base64.b64decode(payload['profile_image']))
         payload['profile_image'] = image_path
     else:
-        if os.path.exists(image_path):
-            os.remove(image_path)
+        for type in ['jpg', 'png', 'jpeg']:
+            if os.path.exists(f'{image_path}.{type}'):
+                os.remove(f'{image_path}.{type}')
 
     authentication.update_user(g.user_id, payload, ('/' + image_path) if profile_image else None)
     user = authentication.get_user(g.user_id)
